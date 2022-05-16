12a3da0d7c6d6256161514097492bf7f888ba36e
jod35 <jodestrevin@gmail.com>
2022-05-16 22:29:45 +0300

    serializers

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
+from pickle import TRUE
 from django.db import models
 
 # Create your models here.
+
+"""
+class Post:
+    id int
+    title str(50)
+    content text
+    created datetime
+"""
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
