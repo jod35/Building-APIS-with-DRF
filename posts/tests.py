3e00dc9c35277679b84aef74cdec409e6202255c
jod35 <jodestrevin@gmail.com>
2022-06-09 00:56:51 +0300

    tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,62 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIRequestFactory
+from .views import PostListCreateView
+from django.urls import reverse
+from rest_framework import status
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+User = get_user_model()
+
+
+class HelloWorldTestCase(APITestCase):
+    def test_hello_world(self):
+        response = self.client.get(reverse("posts_home"))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["message"], "Hello World")
+
+
+class PostListCreateTestCase(APITestCase):
+    def setUp(self):
+        self.url = reverse("list_posts")
+
+    def authenticate(self):
+        self.client.post(
+            reverse("signup"),
+            {
+                "email": "jonathan@app.com",
+                "password": "password##!123",
+                "username": "jonathan",
+            },
+        )
+
+        response = self.client.post(
+            reverse("login"),
+            {
+                "email": "jonathan@app.com",
+                "password": "password##!123",
+            },
+        )
+
+        # print(response.data)
+
+        token = response.data["tokens"]["access"]
+
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
+
+    def test_list_posts(self):
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["count"], 0)
+        self.assertEqual(response.data["results"], [])
+
+    def test_post_creation(self):
+        self.authenticate()
+
+        sample_data = {"title": "Sample title", "content": "Sample content"}
+        response = self.client.post(reverse("list_posts"), sample_data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["title"], sample_data["title"])
